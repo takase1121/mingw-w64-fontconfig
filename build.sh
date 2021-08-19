5b6257aa50f341ff342d362d6e4fccde6bb0c299
Takase <20792268+takase1121@users.noreply.github.com>
2021-08-20 06:52:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,13 @@
 curl -L https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.xz | xz -d | tar -xf -
 curl -L https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.94.tar.xz | xz -d | tar -xf -
 
+mkdir build
+BUILD_DIR=$PWD/build
+
 cd freetype-2.11.0
-./configure --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no --prefix=../build
+./configure --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no --prefix=$BUILD_DIR
 make && make install
 cd ..
 cd fontconfig-2.13.94
-meson build -Ddoc=disabled -Dnls=disabled -Dtests=disabled -Dtools=enabled --prefix=../build
+PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig meson build -Ddoc=disabled -Dnls=disabled -Dtests=disabled -Dtools=enabled --prefix=$BUILD_DIR
 ninja -C build install
