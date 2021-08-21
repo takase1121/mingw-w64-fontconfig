45117adcbaf541a1b3555e6db66bc59221175033
takase1121 <20792268+takase1121@users.noreply.github.com>
2021-08-21 23:18:02 +0800

    big big refactor

--- build.sh
+++ build.sh
@@ -1,42 +1,78 @@
 #!/bin/bash
 
-if ! [ -d "freetype-2.11.0" ]; then
-	curl -sL https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.xz | xz -d | tar -xf -
-fi
-if ! [ -d "fontconfig-2.13.94" ]; then
-	curl -sL https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.94.tar.xz | xz -d | tar -xf -
-fi
+patch_src() {
+	local fullname="$1"
+	local name="${1%%-*}"
+	if [[ -d "patches/$name" ]]; then
+		for f in "patches/$name"/*.patch; do
+			patch -p1 -N --dry-run --silent -d "$fullname" < "$f"
+			if [[ $? -eq 0 ]]; then
+				patch -p1 -d "$fullname" < "$f"
+				if [[ $? -ne 0 ]]; then
+					return 1
+				fi
 
-if ! [ -d "expat-2.4.1" ]; then
-	curl -sL https://github.com/libexpat/libexpat/releases/download/R_2_4_1/expat-2.4.1.tar.xz | xz -d | tar -xf -
-fi
+			fi
+		done
+	fi
+}
 
-if ! [ -d "build" ]; then mkdir build; fi
-BUILD_DIR=$PWD/build
+build_src() {
+	local name="${1%%-*}"
+	cd "$1"
+	eval "build_$name"
+	cd ..
+}
 
-cd freetype-2.11.0
-./configure --with-zlib=no --with-bzip2=no --with-png=no --with-harfbuzz=no --with-brotli=no --enable-static=no --prefix=$BUILD_DIR
-make && make install
-cd ..
+download_src() {
+	local url="$1"
+	local name="$(basename "$url" ".tar.gz")"
+	if ! [ -d "$name" ]; then
+		curl -sL "$url" | tar -xzf -
+		patch_src "$name"
+	fi
+}
 
-cd expat-2.4.1
-./configure --prefix=$BUILD_DIR --without-xmlwf --without-examples --without-tests --without-docbook
-make && make install
-cd ..
+build() {
+	local url="$1"
+	local name="$(basename "$url" ".tar.gz")"
+	download_src "$url"
+	build_src "$name"
+}
 
-cd fontconfig-2.13.94
+build_freetype() {
+	./configure --prefix=$BUILD_DIR \
+		--with-zlib=no --with-bzip2=no --with-png=no \
+		--with-harfbuzz=no --with-brotli=no --enable-static=no
+	make -j"$JOBCOUNT" install
+}
 
-patch -p1 -N --dry-run --silent < ../patches/fc-cache-opt.diff 2>/dev/null
-if [[ $? -eq 0 ]]; then
-	patch -p1 < ../patches/fc-cache-opt.diff
-fi
+build_expat() {
+	./configure --prefix=$BUILD_DIR \
+		--without-xmlwf --without-examples --without-tests --without-docbook
+	make -j"$JOBCOUNT" install
+}
 
-PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig CFLAGS=-static-libgcc CXXFLAGS=-static-libstdc++ meson build -Ddoc=disabled -Dnls=disabled -Dtests=disabled -Dtools=enabled -Dfc-cache=disabled --prefix=$BUILD_DIR
-ninja -C build install
-cd ..
+build_fontconfig() {
+	PKG_CONFIG_PATH=$BUILD_DIR/lib/pkgconfig \
+	CFLAGS=-static-libgcc CXXFLAGS=-static-libstdc++ \
+	meson build --prefix=$BUILD_DIR \
+		-Ddoc=disabled -Dnls=disabled \
+		-Dtests=disabled -Dtools=enabled -Dfc-cache=disabled
+	ninja -C build install
+}
+
+if [[ -d "build" ]]; then rm -rf build; fi
+mkdir build
+BUILD_DIR="$PWD/build"
+JOBCOUNT=`nproc`
+
+build "https://download.savannah.gnu.org/releases/freetype/freetype-2.11.0.tar.gz"
+build "https://github.com/libexpat/libexpat/releases/download/R_2_4_1/expat-2.4.1.tar.gz"
+build "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.94.tar.gz"
 
 # strip everything
-for f in build/bin/*.dll; do
+for f in "$BUILD_DIR/bin"/*{.dll,.exe}; do
 	strip --strip-unneeded "$f"
 done
 
